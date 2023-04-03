e793a8039b82a8e2965f56dc223f2c6df8d1dc41
kyrixn <keyrixn@gmail.com>
2023-04-04 01:03:41 +0800

    introduce PCL as dependency

--- src/pnt_trans.cc
+++ src/pnt_trans.cc
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
 
 ros::Subscriber sub;
 ros::Publisher pub;
