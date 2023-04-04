c054c16e4efa6f021901f9f8428b7a6ed636c749
kyrixn <keyrixn@gmail.com>
2023-04-05 01:14:02 +0800

    addded basic filter to remove outliers

--- src/pnt_trans.cc
+++ src/pnt_trans.cc
@@ -2,21 +2,47 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
+#include <pcl/impl/point_types.hpp>
 #include <pcl/point_types.h>
+#include <pcl/filters/radius_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
 
 ros::Subscriber sub;
 ros::Publisher pub;
 
-sensor_msgs::PointCloud2 pntCloud;
+// void publishPnt(sensor_msgs::PointCloud2 pc) {
+//     pub.publish(pc);
+// }
 
-void publishPnt(sensor_msgs::PointCloud2 pc) {
-    pub.publish(pc);
-}
+// void initROR() {
+//     ror.setInputCloud(pcl_cloud);    
 
-void pntCallback(const sensor_msgs::PointCloud2 cloud)
+//     ror.setRadiusSearch(1.5);
+//     ror.setMinNeighborsInRadius(5);
+// }
+
+void pntCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-    pntCloud = cloud;
-    publishPnt(pntCloud);
+    // Container for original & filtered data
+    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+    pcl::PCLPointCloud2 cloud_filtered;
+
+    // Convert to PCL data type
+    pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+    // Perform the actual filtering
+    pcl::RadiusOutlierRemoval<pcl::PCLPointCloud2> ror;
+    ror.setInputCloud (cloudPtr);
+    ror.setRadiusSearch(0.1);
+    ror.setMinNeighborsInRadius(5);
+    ror.filter (cloud_filtered);
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output;
+    pcl_conversions::fromPCL(cloud_filtered, output);
+
+    // Publish the data
+    pub.publish (output);
 }
 
 int main(int argc, char **argv)
@@ -24,9 +50,12 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "pnt_trans");
     ros::NodeHandle nh;
 
-    sub = nh.subscribe("/orb_slam3/tracked_points", 1, pntCallback);
+    // initROR();
+
+    ROS_INFO("Start\n");
+
+    sub = nh.subscribe("/orb_slam3/all_points", 1, pntCallback);
     pub = nh.advertise<sensor_msgs::PointCloud2>("/transformed_points", 1);
 
     ros::spin();
-    return 0;
 }
